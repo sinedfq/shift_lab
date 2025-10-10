4e07f3a146b9014c7b0e9cdcc574b41e6d22f6cf
Denis <deniskhuharev@gmail.com>
2025-10-10 12:35:19 +0700

    Add api requests, fix phone input

--- src/scripts/formatPhoneNumber.tsx
+++ src/scripts/formatPhoneNumber.tsx
@@ -1,14 +1,25 @@
 export const formatPhoneNumber = (value: string): string => {
-    const numbers = value.replace(/\D/g, '');
-
-    if (numbers.length === 0) return '';
-    if (numbers.length < 1) return `+7${numbers}`;
-    if (numbers.length <= 4) return `+7 (${numbers.slice(1, 4)}`;
-    if (numbers.length <= 7) return `+7 (${numbers.slice(1, 4)}) ${numbers.slice(4, 7)}`;
-    if (numbers.length <= 9) return `+7 (${numbers.slice(1, 4)}) ${numbers.slice(4, 7)}-${numbers.slice(7, 9)}`;
-
-    return `+7 (${numbers.slice(1, 4)}) ${numbers.slice(4, 7)}-${numbers.slice(7, 9)}-${numbers.slice(9, 11)}`;
+    let numbers = value.replace(/\D/g, '');
+    if (numbers.length === 1 && numbers[0] === '9') {
+        numbers = '7' + numbers;
+    }
+    if (numbers === '') return '';
+    let formatted = '+7';
+    if (numbers.length > 1) {
+        formatted += ' (' + numbers.slice(1, 4);
+    }
+    if (numbers.length >= 5) {
+        formatted += ') ' + numbers.slice(4, 7);
+    }
+    if (numbers.length >= 8) {
+        formatted += '-' + numbers.slice(7, 9);
+    }
+    if (numbers.length >= 10) {
+        formatted += '-' + numbers.slice(9, 11);
+    }
+    return formatted;
 };
+
 
 export const formatCode = (value: string): string => {
     const numbers = value.replace(/\D/g, '');
